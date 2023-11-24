93b117e195c05a81cf588a57c63c2c2b47a6faae
davidmurage <davidmurage415@gmail.com>
2023-11-24 22:04:41 +0300

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cors({
       origin: ["http://localhost:3000",
-      "https://trip-app-wiiu.onrender.com/"
+      "https://trip-mern-app.onrender.com/"
  ]
 }))
 app.use(cookieParser());
