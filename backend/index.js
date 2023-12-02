ce83c68521d66eb4f4a3dc8457590830c52798c5
davidmurage <davidmurage415@gmail.com>
2023-12-02 18:57:51 +0300

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ import bookingRoute from './routes/bookings.js';
 
 dotenv.config();
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 8000;
 const corsOptions = {
    origin: true,
